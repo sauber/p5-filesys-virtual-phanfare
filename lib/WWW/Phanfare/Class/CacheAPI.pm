1a1c304795f1f134bfc8a72ab5d1a1046a413762
Soren Dossing <netcom@sauber.net>
2011-01-15 15:26:43 +0900

    Expire cache of albumlist when new album is created

--- lib/WWW/Phanfare/Class/CacheAPI.pm
+++ lib/WWW/Phanfare/Class/CacheAPI.pm
@@ -30,13 +30,23 @@ sub AUTOLOAD {
   my $cachestring = join ',', $method, @_;
   my $result = $CACHE->thaw( $cachestring );
   unless ( $result ) {
-    #warn "*** Caching $cachestring\n";
+    warn "*** Caching $cachestring\n";
     my $super = "SUPER::$method";
     $result = $self->$super( @_ );
     #warn "*** result is " . Dumper $result;
     $CACHE->freeze( $cachestring, $result );
+
+    # Delete cached parent results when creating new objects
+    # *** Caching NewAlbum,target_uid,9497612,album_name,Test2,album_start_date,1999-01-01T00:00:00,album_end_date,1999-12-31T23:59:59
+    # *** Reusing GetAlbumList,target_uid,9497612
+    if ( $method eq 'NewAlbum' ) {
+      my $parent = join ',', 'GetAlbumList', @_[0..1];
+      warn "*** Expiring $parent\n";
+      $CACHE->remove( $parent );
+      $CACHE->purge();  # Also take the opportunity to remove all expired objects
+    }
   } else {
-    #warn "*** Reusing $cachestring";
+    warn "*** Reusing $cachestring\n";
   }
   return $result;
 }
